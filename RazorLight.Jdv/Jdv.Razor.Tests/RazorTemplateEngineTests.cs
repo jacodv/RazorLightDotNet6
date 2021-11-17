299f0e0e483aa9c13d3e997ed83445e2aabbb605
Jaco de Villiers <jacodv@gmail.com>
2021-11-17 06:36:17 +0200

    Added sample method to show it is not DLL hell

--- RazorLight.Jdv/Jdv.Razor.Tests/RazorTemplateEngineTests.cs
+++ RazorLight.Jdv/Jdv.Razor.Tests/RazorTemplateEngineTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -6,6 +10,7 @@ namespace Jdv.Razor.Tests
 	public class RazorTemplateEngineTests
 	{
 		private RazorTemplateEngine _engine;
+
 		[SetUp]
 		public void Setup()
 		{
@@ -31,8 +36,42 @@ namespace Jdv.Razor.Tests
 			// action
 			var result = _engine.Convert(data, template, true);
 
+			var signedResult = Sign(Encoding.UTF8.GetBytes(result), X509Certificate2.CreateFromPemFile("personal.pem"));
+
 			// assert
-			result .Should().Be("Name is TheName");
+			result.Should().Be("Name is TheName");
+		}
+
+		public static byte[] Sign(byte[] message, X509Certificate2 cert)
+	{
+		byte[] signed;
+
+		try
+		{
+			// Se pone el Mensaje recibido en un objeto ContentInfo                 
+			ContentInfo infoContenidoMsj = new ContentInfo(message);
+			// Se instancia el CMS Firmado con el ContentInfo
+			SignedCms cmsFirmado = new SignedCms(infoContenidoMsj);
+			// Se instancia el objeto CmsSigner con las caracteristicas del firmante 
+			CmsSigner cmsFirmante = new CmsSigner(cert);
+
+			cmsFirmante.IncludeOption = X509IncludeOption.EndCertOnly;
+
+			// Se firma el mensaje PKCS #7 con el certificado
+			cmsFirmado.ComputeSignature(cmsFirmante);
+
+			signed = cmsFirmado.Encode();
+
+			// Retorno el mensaje PKCS #7 firmado . 
+			return signed;
+		}
+		catch (Exception excepcionAlFirmar)
+		{
+			throw new Exception(
+				"ERROR: Procedimiento: FirmarMensaje. Al intentar firmar el mensaje con el certificado del firmante: " +
+				excepcionAlFirmar.Message);
 		}
 	}
+}
+
 }
